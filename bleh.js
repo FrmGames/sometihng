3d49da158b2048b57f7d1ff0ed9d40bf611b351a
frgameslol <107143881+FrmGames@users.noreply.github.com>
2023-05-24 18:28:13 -0400

    Update bleh.js
    a

--- bleh.js
+++ bleh.js
@@ -1,1 +1,6 @@
-
+javascript:(function() {
+  var currentUrl = window.location.href;
+  if (currentUrl.indexOf('google.com') !== -1) {
+    window.location.href ='https://www.bing.com';
+  }
+ })();
